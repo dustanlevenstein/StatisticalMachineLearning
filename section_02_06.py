914a6d3eaf750226a155092ef420a97a7d4c4b71
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-07 14:20:51 -0500

    Next question: what does findall(s) look like without [0]?

---
+++ section_02_06.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 14:13:55 2020
+
+@author: dustan
+"""
+
+import re
+regex = re.compile("^.+(sub-.+)_(ses-.+)_(mod-.+)")
+strings = ["abcsub-033_ses-01_mod-mri", "defsub-044_ses-01_mod-mri", "ghisub-055_ses-02_mod-ctscan" ]
+print([regex.findall(s)[0] for s in strings])
