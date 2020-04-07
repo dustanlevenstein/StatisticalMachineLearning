95b1ac6a28112db382047d89143ff228b3f780ec
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-07 14:38:48 -0500

    glob

--- section_02_07_03.py
+++ section_02_07_03.py
@@ -11,3 +11,8 @@ WD = os.path.join(os.environ["HOME"], "git", "pystatsml", "datasets")
 print(WD)
 for dirpath, dirnames, filenames in os.walk(WD):
     print(dirpath, dirnames, filenames)
+    
+import glob
+filenames = glob.glob(os.path.join(os.environ["HOME"], "Dropbox", "*",  "*.html"))
+basenames = [os.path.splitext(os.path.basename(f))[0] for f in filenames]
+print(basenames)
