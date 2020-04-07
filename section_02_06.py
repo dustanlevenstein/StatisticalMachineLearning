b8ded220b79136d0acec19353910fc0947d0f955
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-07 14:21:34 -0500

    Apparently it's just a singleton list.

--- section_02_06.py
+++ section_02_06.py
@@ -9,4 +9,4 @@ Created on Tue Apr  7 14:13:55 2020
 import re
 regex = re.compile("^.+(sub-.+)_(ses-.+)_(mod-.+)")
 strings = ["abcsub-033_ses-01_mod-mri", "defsub-044_ses-01_mod-mri", "ghisub-055_ses-02_mod-ctscan" ]
-print([regex.findall(s)[0] for s in strings])
+print([regex.findall(s) for s in strings])
