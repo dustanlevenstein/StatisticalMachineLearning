6876532cdd0b9385a94b451e2429bfcaf2941cd7
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-07 14:29:36 -0500

    Not sure of the os.makedirs did what I thought it did, but I'm not going to worry about it.

--- section_02_07_01.py
+++ section_02_07_01.py
@@ -9,3 +9,9 @@ Created on Tue Apr  7 14:24:56 2020
 import tempfile
 tmpdir = tempfile.gettempdir()
 print(tmpdir)
+
+import os
+mytmpdir = os.path.join(tmpdir, "foobar")
+print(os.listdir(tmpdir))
+os.makedirs(os.path.join(tmpdir, "foobar", "plop", "toto"))
+print(tmpdir)
