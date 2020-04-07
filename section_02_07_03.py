767dd84ea4f8ccb9004f44d9c619ad572f96775c
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-07 14:32:48 -0500

    Walking a (nonexistent, in my case) directory.

---
+++ section_02_07_03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 14:30:05 2020
+
+@author: dustan
+"""
+
+import os
+WD = os.path.join(os.environ["HOME"], "git", "pystatsml", "datasets")
+print(WD)
+for dirpath, dirnames, filenames in os.walk(WD):
+    print(dirpath, dirnames, filenames)
