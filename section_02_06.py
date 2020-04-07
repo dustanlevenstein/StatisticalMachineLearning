9e83e8309ca0e38ea643d5d3e58b6079b441f260
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-07 14:24:20 -0500

    hello world.

--- section_02_06.py
+++ section_02_06.py
@@ -10,3 +10,9 @@ import re
 regex = re.compile("^.+(sub-.+)_(ses-.+)_(mod-.+)")
 strings = ["abcsub-033_ses-01_mod-mri", "defsub-044_ses-01_mod-mri", "ghisub-055_ses-02_mod-ctscan" ]
 print([regex.findall(s) for s in strings])
+
+regex = re.compile("(sub-[^_]+)")
+print([regex.sub("SUB-", s) for s in strings])
+
+print(regex.sub("SUB-", "toto"))
+print(re.sub('[^0-9a-zA-Z]+', '', 'h^&ell`.,|o w]{+orld'))
