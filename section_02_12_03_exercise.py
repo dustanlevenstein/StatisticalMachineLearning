868831b7954767c653174c4af94657e19f0ad73e
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-07 15:19:38 -0500

    Completed first part of exercise.

--- section_02_12_03_exercise.py
+++ section_02_12_03_exercise.py
@@ -6,3 +6,14 @@ Created on Tue Apr  7 15:10:12 2020
 @author: dustan
 """
 
+import re
+
+def parse_word_count(filename = "section_02_12_03_bsd4clause.txt"):
+    regex = re.compile("[a-zA-Z]+")
+    f = open(filename, "r")
+    count = dict()
+    for line in f:
+        for word in regex.findall(line.lower()):
+            count[word] = count.get(word, 0) + 1
+    f.close()
+    return count
