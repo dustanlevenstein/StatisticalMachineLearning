ed2112be638b9c5dca6021d91e0be24aa3e33f9c
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-07 16:08:56 -0500

    FIrst plot

---
+++ section_03_03_01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 16:03:48 2020
+
+@author: dustan
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 10, 50)
+sinus = np.sin(x)
+cosinus = np.cos(x)
+plt.plot(x, sinus, "-b", x, sinus, "ob", x, cosinus, "-r", x, cosinus, "or")
+plt.xlabel("this is x factorial")
+plt.ylabel("this is y!")
+plt.title("Hello woild!")
+plt.show()
