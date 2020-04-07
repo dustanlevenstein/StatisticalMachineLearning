a37fce6b683d72f576ad4800bc203350b052ca76
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-07 15:08:35 -0500

    Completed exercise.

---
+++ section_02_12_02_exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 15:06:04 2020
+
+@author: dustan
+"""
+
+def delete_adj_duplicates(li):
+    previous = None
+    result = []
+    for ii in li:
+        if ii != previous:
+            result.append(ii)
+    return result
+def delete_all_duplicates(li):
+    previous = set()
+    result = []
+    for ii in li:
+        if ii not in previous:
+            result.append(ii)
+            previous.add(ii)
+    return result
