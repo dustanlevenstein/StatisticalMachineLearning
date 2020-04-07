916cf1353956815ed3265a9201e92f8435ce840b
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-07 14:44:38 -0500

    Straight from the horse's mouth.

---
+++ section_02_07_05.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 14:33:07 2020
+
+@author: dustan
+"""
+
+import time
+import threading
+def list_append(count, sign=1, out_list=None):
+    if out_list is None:
+        out_list = list()
+    for i in range(count):
+        out_list.append(sign * i)
+        sum(out_list) # do some computation
+    return out_list
+size = 10000 # Number of numbers to add
+out_list = list() # result is a simple list
+thread1 = threading.Thread(target=list_append, args=(size, 1, out_list, ))
+thread2 = threading.Thread(target=list_append, args=(size, -1, out_list, ))
+startime = time.time()
+# Will execute both in parallel
+thread1.start()
+thread2.start()
+# Joins threads back to the parent process
+thread1.join()
+thread2.join()
+print("Threading ellapsed time ", time.time() - startime)
+print(out_list[:10])
