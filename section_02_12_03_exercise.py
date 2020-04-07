5a6325d7333fbd1e8db741fe01ae39e0e2e758fe
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-07 15:24:07 -0500

    I think this is all that's required for part 2.

--- section_02_12_03_exercise.py
+++ section_02_12_03_exercise.py
@@ -17,3 +17,42 @@ def parse_word_count(filename = "section_02_12_03_bsd4clause.txt"):
             count[word] = count.get(word, 0) + 1
     f.close()
     return count
+
+import argparse
+import os
+import os.path
+import pandas as pd
+def to_file():
+    # parse command line options
+    output = "word_count.csv"
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input',
+                        help='list of input files.',
+                        nargs='+', type=str)
+    parser.add_argument('-o', '--output',
+                        help='output csv file (default %s)' % output,
+                        type=str, default=output)
+    options = parser.parse_args()
+    if options.input is None :
+        parser.print_help()
+        raise SystemExit("Error: input files are missing")
+    else:
+        filenames = [f for f in options.input if os.path.isfile(f)]
+    # Match words
+    regex = re.compile("[a-zA-Z]+")
+    count = dict()
+    for filename in filenames:
+        fd = open(filename, "r")
+        for line in fd:
+            for word in regex.findall(line.lower()):
+                if not word in count:
+                    count[word] = 1
+                else:
+                    count[word] += 1
+    fd = open(options.output, "w")
+    # Pandas
+    df = pd.DataFrame([[k, count[k]] for k in count], columns=["word", "count"])
+    df.to_csv(options.output, index=False)
+    
+if __name__ == "__main__":
+    to_file()
