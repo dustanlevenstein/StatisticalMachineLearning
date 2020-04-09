08df95fa0b36b87aa52076f004a8f83f273891ce
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-09 13:55:37 -0500

    Chi2 distribution.

--- section_04_01_02.py
+++ section_04_01_02.py
@@ -21,3 +21,15 @@ plt.plot(x, norm.pdf(x, mu, sigma))
 sample_normal = np.random.normal(0, sigma, 100)
 sns.distplot(sample_normal)
 plt.show()
+
+
+
+# Chi square distribution
+
+cvalues = np.linspace(.1, 5, 100)
+from scipy.stats import chi2
+plt.plot(cvalues, chi2.pdf(cvalues, 1), 'b-', label="Chi2(1)")
+plt.plot(cvalues, chi2.pdf(cvalues, 5), 'r-', label="Chi2(5)")
+plt.plot(cvalues, chi2.pdf(cvalues, 30), 'g-', label="Chi2(30)")
+plt.legend()
+plt.show()
