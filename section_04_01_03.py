985d8ba2c6d248b16311df1d205d68894340c4ed
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-09 14:42:28 -0500

    There's probably a better way to compare density and CDF but here.

--- section_04_01_03.py
+++ section_04_01_03.py
@@ -8,14 +8,13 @@ Created on Thu Apr  9 14:23:51 2020
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats
 
 plt.bar([0, 1, 2, 3], [1/8, 3/8, 3/8, 1/8], width=0.9)
 _ = plt.xticks([0, 1, 2, 3], [0, 1, 2, 3])
 plt.xlabel("Distribution of the number of head over 3 flip under the null hypothesis")
 plt.show()
 
-import scipy.stats
-import matplotlib.pyplot as plt
 #tobs = 2.39687663116 # assume the t-value
 succes = np.linspace(30, 70, 41)
 plt.plot(succes, scipy.stats.binom.pmf(succes, 100, 0.5), 'b-',
@@ -27,4 +26,12 @@ plt.fill_between(upper_succes_tvalues, 0,
 _ = plt.legend()
 pval = 1 - scipy.stats.binom.cdf(60, 100, 0.5)
 print(pval)
+plt.show()
+
+x = np.linspace(0, 100, 101)
+plt.plot(x, 12*scipy.stats.binom.pmf(x, 100, 0.5), 'b-',
+         label="Binomial(100, 0.5) approx density")
+plt.plot(x, scipy.stats.binom.cdf(x, 100, 0.5), 'r-',
+         label="Binomial(100, 0.5) CDF")
+plt.legend()
 plt.show()
