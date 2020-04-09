0e06cd41835bda7ab46e70fe43917dcff042879d
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-09 12:14:50 -0500

    Figure with multiple graphs.

--- section_03_03_04.py
+++ section_03_03_04.py
@@ -7,7 +7,7 @@ Created on Thu Apr  9 12:04:55 2020
 """
 
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 url = 'https://raw.github.com/neurospin/pystatsml/master/datasets/salary_table.csv'
@@ -19,3 +19,16 @@ sns.boxplot(x="education", y="salary", hue="management", data=salary)
 sns.boxplot(x="management", y="salary", hue="education", data=salary)
 sns.stripplot(x="management", y="salary", hue="education", data=salary,
               jitter=True, dodge=True, linewidth=1)
+
+# Set up the matplotlib figure: 3 x 1 axis
+f, axes = plt.subplots(3, 1, figsize=(9, 9), sharex=True)
+i = 0
+for edu, d in salary.groupby(['education']):
+    sns.distplot(d.salary[d.management == "Y"], color="b", bins=10,
+                 label="Manager", ax=axes[i])
+    sns.distplot(d.salary[d.management == "N"], color="r", bins=10,
+                 label="Employee", ax=axes[i])
+    axes[i].set_title(edu)
+    axes[i].set_ylabel('Density')
+    i += 1
+ax = plt.legend()
