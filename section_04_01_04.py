4fc8f0a29ababc4b6c2898cd223c8ff4332818b7
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-09 15:05:42 -0500

    Test recommendations, correlated variables.

---
+++ section_04_01_04.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 14:59:34 2020
+
+@author: dustan
+"""
+
+import numpy as np
+import scipy.stats as stats
+n = 50
+x = np.random.normal(size=n)
+y = 2 * x + np.random.normal(size=n)
+# Compute with scipy
+cor, pval = stats.pearsonr(x, y)
