5a4fef9e0f7e7bfba1973e618b7fb27b89918a8f
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-09 15:06:53 -0500

    Even with a sample size of 10, the null hypothesis is rejected with p-value 0.0017.

--- section_04_01_04.py
+++ section_04_01_04.py
@@ -8,7 +8,7 @@ Created on Thu Apr  9 14:59:34 2020
 
 import numpy as np
 import scipy.stats as stats
-n = 50
+n = 10
 x = np.random.normal(size=n)
 y = 2 * x + np.random.normal(size=n)
 # Compute with scipy
