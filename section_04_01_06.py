2c499dce6d0b5d922d9dce3f7eb9b8e6c770142c
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-09 15:50:44 -0500

    plotting linear regression

--- section_04_01_06.py
+++ section_04_01_06.py
@@ -10,3 +10,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 url = 'https://raw.github.com/neurospin/pystatsml/master/datasets/salary_table.csv'
 salary = pd.read_csv(url)
+
+from scipy import stats
+import numpy as np
+y, x = salary.salary, salary.experience
+beta, beta0, r_value, p_value, std_err = stats.linregress(x,y)
+print("y = %f x + %f, r: %f, r-squared: %f,\np-value: %f, std_err: %f"
+      % (beta, beta0, r_value, r_value**2, p_value, std_err))
+print("Regression line with the scatterplot")
+yhat = beta * x + beta0 # regression line
+
+plt.plot(x, yhat, 'r-', x, y,'o')
+plt.xlabel('Experience (years)')
+plt.ylabel('Salary')
+plt.show()
+print("Using seaborn")
+import seaborn as sns
+sns.regplot(x="experience", y="salary", data=salary)
+plt.show()
+
