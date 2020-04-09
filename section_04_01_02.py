7ec070ed593ee27b7926da95817111bcdd2cf00e
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-09 13:40:00 -0500

    Showing sample distribution as well.

--- section_04_01_02.py
+++ section_04_01_02.py
@@ -10,9 +10,14 @@ Created on Thu Apr  9 12:41:01 2020
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+import seaborn as sns
 
 mu = 0 # mean
 variance = 2 #variance
 sigma = np.sqrt(variance) #standard deviation\n",
 x = np.linspace(mu-3*variance,mu+3*variance, 100)
 plt.plot(x, norm.pdf(x, mu, sigma))
+
+sample_normal = np.random.normal(0, sigma, 100)
+sns.distplot(sample_normal)
+plt.show()
