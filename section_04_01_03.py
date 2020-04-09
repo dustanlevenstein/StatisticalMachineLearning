6add4d22017201b1f8f80b8509f3122882e886d8
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-09 14:32:05 -0500

    test whether coin with 60 heads out of 100 is biased.

--- section_04_01_03.py
+++ section_04_01_03.py
@@ -7,8 +7,24 @@ Created on Thu Apr  9 14:23:51 2020
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 plt.bar([0, 1, 2, 3], [1/8, 3/8, 3/8, 1/8], width=0.9)
 _ = plt.xticks([0, 1, 2, 3], [0, 1, 2, 3])
 plt.xlabel("Distribution of the number of head over 3 flip under the null hypothesis")
-#Text(0.5, 0, 'Distribution of the number of head over 3 flip under the null hypothesis')
+plt.show()
+
+import scipy.stats
+import matplotlib.pyplot as plt
+#tobs = 2.39687663116 # assume the t-value
+succes = np.linspace(30, 70, 41)
+plt.plot(succes, scipy.stats.binom.pmf(succes, 100, 0.5), 'b-',
+         label="Binomial(100, 0.5)")
+upper_succes_tvalues = succes[succes > 60]
+plt.fill_between(upper_succes_tvalues, 0,
+                 scipy.stats.binom.pmf(upper_succes_tvalues, 100, 0.5),
+                 alpha=.8, label="p-value")
+_ = plt.legend()
+pval = 1 - scipy.stats.binom.cdf(60, 100, 0.5)
+print(pval)
+plt.show()
