59e6bc8640596621a3bcf35616bc891941244c7f
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-11 12:19:20 -0500

    Legend is empty...

--- section_04_03_08_exercises.py
+++ section_04_03_08_exercises.py
@@ -19,3 +19,18 @@ print(euclidean(a-b)) # should be 1
 
 
 print(((a.T @ b)/euclidean(a))[0,0])
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+colors = sns.color_palette()
+
+plt.arrow(0, 0, a[0,0], a[1, 0], head_width=.1, color=colors[0], label="a")
+plt.arrow(0, 0, b[0,0], b[1, 0], head_width=.1, color=colors[1], label="b")
+plt.xlim(-3, 3)
+plt.ylim(-3, 3)
+plt.legend()
+plt.show()
+
+np.random.seed(42)
+
+X = np.random.random((100, 2, 1))*4-2
