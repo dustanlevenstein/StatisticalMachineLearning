c232f80dc5516f98443809243182120cff72cb53
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-10 20:03:54 -0500

    Larger sample, white ellipse, removed mean centers, moved clumps a little bit further apart (but not so far that they don't overlap).

--- section_04_03_03.py
+++ section_04_03_03.py
@@ -17,22 +17,22 @@ import seaborn as sns  # nice color
 np.random.seed(42)
 colors = sns.color_palette()
 
-n_samples, n_features = 100, 2
+n_samples, n_features = 100000, 2
 
 mean, Cov, X = [None] * 4, [None] * 4, [None] * 4
-mean[0] = np.array([-2.5, 2.5])
+mean[0] = np.array([-3.5, 3.5])
 Cov[0] = np.array([[1, 0],
                    [0, 1]])
 
-mean[1] = np.array([2.5, 2.5])
+mean[1] = np.array([3.5, 3.5])
 Cov[1] = np.array([[1, .5],
                    [.5, 1]])
 
-mean[2] = np.array([-2.5, -2.5])
+mean[2] = np.array([-3.5, -3.5])
 Cov[2] = np.array([[1, .9],
                    [.9, 1]])
 
-mean[3] = np.array([2.5, -2.5])
+mean[3] = np.array([3.5, -3.5])
 Cov[3] = np.array([[1, -.9],
                    [-.9, 1]])
 
@@ -45,11 +45,11 @@ for i in range(len(mean)):
     # Points
     plt.scatter(X[i][:, 0], X[i][:, 1], color=colors[i], label="class %i" % i)
     # Means
-    plt.scatter(mean[i][0], mean[i][1], marker="o", s=200, facecolors='w',
-                edgecolors=colors[i], linewidth=2)
+    #plt.scatter(mean[i][0], mean[i][1], marker="o", s=200, facecolors='w',
+    #            edgecolors=colors[i], linewidth=2)
     # Ellipses representing the covariance matrices
     plot_utils.plot_cov_ellipse(Cov[i], pos=mean[i], facecolor='none',
-                                          linewidth=2, edgecolor=colors[i])
+                                          linewidth=1, edgecolor='w')
 
 plt.axis('equal')
 _ = plt.legend(loc='upper left')
