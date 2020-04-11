c1ad3c970f290faabbf5da83506684a4b04934d6
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-11 11:10:12 -0500

    Ripped code. Need to remove references to pystatsml.

--- section_04_03_06.py
+++ section_04_03_06.py
@@ -23,3 +23,58 @@ d_euc = np.sqrt(np.dot(ones, ones))
 d_mah = np.sqrt(np.dot(np.dot(ones, Prec), ones))
 
 print("Euclidean norm of ones=%.2f. Mahalanobis norm of ones=%.2f" % (d_euc, d_mah))
+
+
+import scipy
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pystatsml.plot_utils
+np.random.seed(40)
+colors = sns.color_palette()
+
+mean = np.array([0, 0]) 
+Cov = np.array([[1, .8],
+                [.8, 1]])
+samples = np.random.multivariate_normal(mean, Cov, 100)
+x1 = np.array([0, 2])
+x2 = np.array([2, 2])
+
+plt.scatter(samples[:, 0], samples[:, 1], color=colors[0])
+plt.scatter(mean[0], mean[1], color=colors[0], s=200, label="mean")
+plt.scatter(x1[0], x1[1], color=colors[1], s=200, label="x1")
+plt.scatter(x2[0], x2[1], color=colors[2], s=200, label="x2")
+
+# plot covariance ellipsis
+pystatsml.plot_utils.plot_cov_ellipse(Cov, pos=mean, facecolor='none', 
+                                      linewidth=2, edgecolor=colors[0])
+# Compute distances
+d2_m_x1 = scipy.spatial.distance.euclidean(mean, x1)
+d2_m_x2 = scipy.spatial.distance.euclidean(mean, x2)
+
+Covi = scipy.linalg.inv(Cov)
+dm_m_x1 = scipy.spatial.distance.mahalanobis(mean, x1, Covi)
+dm_m_x2 = scipy.spatial.distance.mahalanobis(mean, x2, Covi)
+
+# Plot distances
+vm_x1 = (x1 - mean) / d2_m_x1
+vm_x2 = (x2 - mean) / d2_m_x2
+jitter = .1
+plt.plot([mean[0] - jitter, d2_m_x1 * vm_x1[0] - jitter],
+         [mean[1], d2_m_x1 * vm_x1[1]], color='k')
+plt.plot([mean[0] - jitter, d2_m_x2 * vm_x2[0] - jitter],
+         [mean[1], d2_m_x2 * vm_x2[1]], color='k')
+
+plt.plot([mean[0] + jitter, dm_m_x1 * vm_x1[0] + jitter],
+         [mean[1], dm_m_x1 * vm_x1[1]], color='r')
+plt.plot([mean[0] + jitter, dm_m_x2 * vm_x2[0] + jitter],
+         [mean[1], dm_m_x2 * vm_x2[1]], color='r')
+
+plt.legend(loc='lower right')
+plt.text(-6.1, 3,
+         'Euclidian:   d(m, x1) = %.1f<d(m, x2) = %.1f' % (d2_m_x1, d2_m_x2), color='k')
+plt.text(-6.1, 3.5,
+         'Mahalanobis: d(m, x1) = %.1f>d(m, x2) = %.1f' % (dm_m_x1, dm_m_x2), color='r')
+
+plt.axis('equal')
+print('Euclidian   d(m, x1) = %.2f < d(m, x2) = %.2f' % (d2_m_x1, d2_m_x2))
+print('Mahalanobis d(m, x1) = %.2f > d(m, x2) = %.2f' % (dm_m_x1, dm_m_x2))
