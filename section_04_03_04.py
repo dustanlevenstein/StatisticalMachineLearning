26b20369328fd3ef4e560ec6f3f9830637fd12d1
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-10 20:19:20 -0500

    Pulled in code. Added plt.show()'s.

---
+++ section_04_03_04.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 20:15:55 2020
+
+@author: dustan
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+url = 'https://python-graph-gallery.com/wp-content/uploads/mtcars.csv'
+df = pd.read_csv(url)
+
+# Compute the correlation matrix
+corr = df.corr()
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+f, ax = plt.subplots(figsize=(5.5, 4.5))
+cmap = sns.color_palette("RdBu_r", 11)
+# Draw the heatmap with the mask and correct aspect ratio
+_ = sns.heatmap(corr, mask=None, cmap=cmap, vmax=1, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+plt.show()
+
+# convert correlation to distances
+d = 2 * (1 - np.abs(corr))
+
+from sklearn.cluster import AgglomerativeClustering
+clustering = AgglomerativeClustering(n_clusters=3, linkage='single', affinity="precomputed").fit(d)
+lab=0
+
+clusters = [list(corr.columns[clustering.labels_==lab]) for lab in set(clustering.labels_)]
+print(clusters)
+
+reordered = np.concatenate(clusters)
+
+R = corr.loc[reordered, reordered]
+
+f, ax = plt.subplots(figsize=(5.5, 4.5))
+# Draw the heatmap with the mask and correct aspect ratio
+_ = sns.heatmap(R, mask=None, cmap=cmap, vmax=1, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+plt.show()
