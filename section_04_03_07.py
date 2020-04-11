9806fa58936a87e7cc135e48adffdda61222230f
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-11 11:22:55 -0500

    code ripped as-is.

---
+++ section_04_03_07.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 11 11:21:49 2020
+
+@author: dustan
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats
+from scipy.stats import multivariate_normal
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def multivariate_normal_pdf(X, mean, sigma):
+    """Multivariate normal probability density function over X (n_samples x n_features)"""
+    P = X.shape[1]
+    det = np.linalg.det(sigma)
+    norm_const = 1.0 / (((2*np.pi) ** (P/2)) * np.sqrt(det))
+    X_mu = X - mu
+    inv = np.linalg.inv(sigma)
+    d2 = np.sum(np.dot(X_mu, inv) * X_mu, axis=1)
+    return norm_const * np.exp(-0.5 * d2)
+
+# mean and covariance
+mu = np.array([0, 0])
+sigma = np.array([[1, -.5],
+                  [-.5, 1]])
+
+# x, y grid
+x, y = np.mgrid[-3:3:.1, -3:3:.1]
+X = np.stack((x.ravel(), y.ravel())).T
+norm = multivariate_normal_pdf(X, mean, sigma).reshape(x.shape)
+
+# Do it with scipy
+norm_scpy = multivariate_normal(mu, sigma).pdf(np.stack((x, y), axis=2))
+assert np.allclose(norm, norm_scpy)
+
+# Plot
+fig = plt.figure(figsize=(10, 7))
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(x, y, norm, rstride=3,
+        cstride=3, cmap=plt.cm.coolwarm,
+        linewidth=1, antialiased=False
+    )
+
+ax.set_zlim(0, 0.2)
+ax.zaxis.set_major_locator(plt.LinearLocator(10))
+ax.zaxis.set_major_formatter(plt.FormatStrFormatter('%.02f'))
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('p(x)')
+
+plt.title('Bivariate Normal/Gaussian distribution')
+fig.colorbar(surf, shrink=0.5, aspect=7, cmap=plt.cm.coolwarm)
+plt.show()
