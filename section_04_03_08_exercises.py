6d5c61c917a0fd5a40611ae77f6f1f34c0011c4d
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-11 12:01:27 -0500

    Needed to use @ for matrix multiplication, since dot product is not suited for column vectors apparently.

---
+++ section_04_03_08_exercises.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 11 11:51:09 2020
+
+@author: dustan
+"""
+
+import numpy as np
+
+a = np.array([[2], [1]])
+b = np.array([[1], [1]])
+
+def euclidean(x):
+    return np.sqrt(np.sum(x*x))
+
+print(euclidean(a)) # should be sqrt(5)
+print(euclidean(a-b)) # should be 1
+
+
+print((a.T @ b)/euclidean(a))
