ac8d5db1df988b4504d49916c03653fdbd5e0788
Dustan Levenstein <dlevenstein@gmail.com>
2020-04-20 16:52:40 -0500

    Copied code in with adjustments.

--- section_05_01_06_exercises.py
+++ section_05_01_06_exercises.py
@@ -8,8 +8,9 @@ Created on Fri Apr 17 17:46:30 2020
 
 import numpy as np
 class BasicPCA(object):
-    def __init__(self, data=None):
+    def __init__(self, n_components=None, data=None):
         self.data = (np.array(data) if data is not None else None)
+        self.numcomponents = n_components
     def fit(self, X=None):
         if X is None:
             X = self.data
@@ -27,14 +28,42 @@ class BasicPCA(object):
         eigenvectors = np.flip(eigenvectors, axis=1)
         eigenvalues = np.flip(eigenvalues)
         
-        self.explained_variances = list(eigenvalues/eigenvalues.sum())
+        self.explained_variance_ratio_ = list(eigenvalues/eigenvalues.sum())
         self.pc_directions = eigenvectors
-    def transform(self, X=None, num_components=2):
-        if X is None:
-            X = self.data
+    def transform(self, num_components=None):
+        if num_components is None:
+            num_components = self.numcomponents
+            if num_components is None:
+                num_components = 2
         else:
             self.data = X
             self.fit()
         
         # TODO
-        
+
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+ 
+# dataset
+n_samples = 100
+experience = np.random.normal(size=n_samples)
+salary = 1500 + experience + np.random.normal(size=n_samples, scale=.5)
+X = np.column_stack([experience, salary])
+
+pca = BasicPCA(n_components=2)
+pca.fit(X)
+print(pca.explained_variance_ratio_)
+
+# PC = pca.transform(X)
+
+# plt.subplot(121)
+# plt.scatter(X[:, 0], X[:, 1])
+# plt.xlabel("x1"); plt.ylabel("x2")
+
+# plt.subplot(122)
+# plt.scatter(PC[:, 0], PC[:, 1])
+# plt.xlabel("PC1 (var=%.2f)" % pca.explained_variance_ratio_[0])
+# plt.ylabel("PC2 (var=%.2f)" % pca.explained_variance_ratio_[1])
+# plt.axis('equal')
+# plt.tight_layout()
